0a67a25986b9a72f1cfb866a1313a2e3941e0da4
Nitesh-DK <nittesh.dk@gmail.com>
2025-02-11 10:38:34 +0600

    understand of langchain is done

---
+++ User-Perspective/Langchain/note.py
@@ -0,0 +1,8 @@
+
+# ? Langchain is an open-source freamework for developing applications porwered by llms 
+#!@ NLU --> natural laangugage understaning 
+# ! Brain --> context aware text generation 
+
+# ? vertor -- sets of numbers  
+
+# ? alternativves of langhcina are lamaindex , and heystack 

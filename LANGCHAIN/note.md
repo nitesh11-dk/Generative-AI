7af1b170e10f05ab7e1c49bc88ef17d067593acc
Nitesh-DK <nittesh.dk@gmail.com>
2025-03-15 14:33:33 +0530

    done

--- LANGCHAIN/note.md
+++ LANGCHAIN/note.md
@@ -1,4 +1,12 @@
 
-
+# MODEL 
 -A chat model in LangChain is a component designed to communicate in a
 structured way with LLMs like GPT-4, Hugging Face, and Claude Sonnet.
+
+# Meesgaes
+1. SystemMessage: Defines the AI's role and sets the context for the
+conversation
+Example. "You are a marketing expert."
+2. HumanMessage: Represents user input or questions directed to the AI
+Example. "What's a good marketing strategy"?
+3. AIMessage: Contains the AI's responses based on previous messages
--- LANGCHAIN/src/Models/app.py
+++ LANGCHAIN/src/Models/app.py
@@ -1,9 +1,16 @@
 from langchain_google_genai import ChatGoogleGenerativeAI
 from dotenv import load_dotenv
+from langchain_core.messages import HumanMessage ,SystemMessage
+
+messages = [
+    SystemMessage("heelo you are my except in threejs , react three fiber library nodejs for creating a 3D websites"),
+    HumanMessage("please tell me how do i create a small sphere inthe canvas using react three fiebr ")
+]
 
 
 load_dotenv()
 
 llm = ChatGoogleGenerativeAI(model="gemini-1.5-pro")
-res = llm.invoke("What is the capital of india ? ")
+# res = llm.invoke("What is the capital of india ? ")
+res = llm.invoke(messages)
 print(res.content)
